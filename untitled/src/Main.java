ebdab97b1e78eb2fc7ad99fb5836f7b305ed257c
rishika1998 <rishikapriya.1306@gmail.com>
2023-05-31 18:48:43 -0500

    hjj

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+                Student student = new Student();
+                student.setName("rishika priya");
+                String name = student.getName();
+                System.out.println("Student name: " + name);
+    }
+}
